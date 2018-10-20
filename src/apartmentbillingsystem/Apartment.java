71ca2e111c962023fbac738752524df89eeed8da
Zekihan <38537758+Zekihan@users.noreply.github.com>
2018-10-20 19:34:43 +0300

    aa

--- src/apartmentbillingsystem/Apartment.java
+++ src/apartmentbillingsystem/Apartment.java
@@ -3,7 +3,16 @@ package apartmentbillingsystem;
 public class Apartment {
 
 	private Flat[][] apartment ;
+	private int numberOfFloors;
+	private int  numberOfFlats;
 
+	public Apartment() {
+		this.numberOfFlats = 0;
+		this.numberOfFloors = 0;
+		this.setApartment(new Flat[numberOfFloors][numberOfFlats]);
+
+	}
+	
 	public Apartment(Flat[][] apartment) {
 		this.setApartment(apartment);
 	}
@@ -15,6 +24,18 @@ public class Apartment {
 	public void setApartment(Flat[][] apartment) {
 		this.apartment = apartment;
 	}
+
+	@Override
+	public String toString() {
+		String printable = "";
+		for (int i = 0;i<numberOfFloors;i++) {
+			for (int j = 0;j<numberOfFlats;j++) {
+				printable += apartment[i][j];
+			}
+		}
+		return ;
+	}
 	
 	
 }
+
