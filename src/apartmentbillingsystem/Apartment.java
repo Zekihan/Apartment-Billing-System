c2e8b5229f663c87eb7ad5b6318470d46f83110c
Zekihan <38537758+Zekihan@users.noreply.github.com>
2018-10-20 19:21:25 +0300

    apartment

--- src/apartmentbillingsystem/Apartment.java
+++ src/apartmentbillingsystem/Apartment.java
@@ -2,4 +2,19 @@ package apartmentbillingsystem;
 
 public class Apartment {
 
+	private Flat[][] apartment ;
+
+	public Apartment(Flat[][] apartment) {
+		this.setApartment(apartment);
+	}
+
+	public Flat[][] getApartment() {
+		return apartment;
+	}
+
+	public void setApartment(Flat[][] apartment) {
+		this.apartment = apartment;
+	}
+	
+	
 }
