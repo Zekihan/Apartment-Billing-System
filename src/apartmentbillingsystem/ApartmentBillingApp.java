b06446588da1fd719ff9d1d44e5bb648d68df88d
Efe Can Cevher <37309169+efe-cevher@users.noreply.github.com>
2018-10-26 11:28:59 +0300

    Update src/apartmentbillingsystem/ApartmentBillingApp.java

--- src/apartmentbillingsystem/ApartmentBillingApp.java
+++ src/apartmentbillingsystem/ApartmentBillingApp.java
@@ -4,13 +4,9 @@ public class ApartmentBillingApp {
 
 	public static void main(String[] args) {
 
-		ApartmentBillingManager apMngr = new ApartmentBillingManager();
-
-		
+		ApartmentBillingManager apMngr = new ApartmentBillingManager();	
 		Apartment ap = apMngr.getApartment();
-		System.out.println(ap.getNumberOfFloors());
-		System.out.println(ap.getApartment()[2][1].getSqrMeter());
-		System.out.println(ap.getApartment()[2][1].toString());
+
 
 		
 	}
